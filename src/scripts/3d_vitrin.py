87bca976cbf217622775440241e43584dd012a38
Voltschok <65617752+Voltschok@users.noreply.github.com>
2023-06-24 11:06:05 +0300

    Update 3d_vitrin.py

--- src/scripts/3d_vitrin.py
+++ src/scripts/3d_vitrin.py
@@ -31,12 +31,44 @@ def get_distance(lat_1, lat_2, long_1, long_2):
 
 udf_func=F.udf(get_distance)
 
-
 def input_paths(date, depth,base_input_path ):
     """ Функция для расчета путей по заданной дате и глубине  """
     dt = datetime.datetime.strptime(date, '%Y-%m-%d')
     return [f"{base_input_path}/date={(dt-datetime.timedelta(days=x)).strftime('%Y-%m-%d')}" for x in range(int(depth))]
 
+
+def get_geo_cities(csv_path, spark):
+    """Функция рассчитывает датасет с геоданными городов из csv-файла""" 
+    geo_data_csv=spark.read.option("header", True)\
+    .option("delimiter", ";").csv(csv_path)
+ 
+    geo_data= geo_data_csv.withColumn('lat', regexp_replace('lat', ',', '.').cast(DoubleType()))\
+    .withColumn('lon', regexp_replace('lng', ',', '.').cast(DoubleType()))\
+    .withColumnRenamed("lat", "lat_c") \
+    .withColumnRenamed("lon", "lon_c")\
+    .select('id', 'city', 'lat_c',  'lon_c')
+  
+    return geo_data
+
+def get_subs_city(common_subs_distance, csv_path, spark): 
+    
+    """ Функция рассчитывает ближайший город и возвращает в виде датасета """
+    cities=get_geo_cities(csv_path, spark) 
+
+    #рассчитываем датасет с информацией по городам, из которых направлены все сообщения 
+    #(используем udf функцию для расчета расстояния)
+    common_subs_distance.printSchema()
+    messages_cities=common_subs_distance\
+    .crossJoin(cities)\
+    .withColumn('distance',udf_func( F.col('lat_1'), F.col('lat_c'), F.col('lon_1'), F.col('lon_c')).cast(DoubleType()))\
+    .withColumn("distance_rank", F.row_number().over(Window().partitionBy(['user_left'])\
+                                                            .orderBy(F.asc("distance")))) \
+    .where("distance_rank == 1")
+    .drop('distance_rank', 'distance')
+    .withColumnRenamed('city', 'zone_id')
+    
+    
+    return messages_cities
 
 
 def main():   
@@ -50,7 +82,7 @@ def main():
     base_input_path='/user/voltschok/data/geo/events'
     date='2022-05-25'
     depth=10
-    csv_path='/user/voltschok/data/geo/cities'
+    csv_path='/user/voltschok/data/geo/test.csv'
     output_path='/user/voltschok/data/geo/analytics/'
     spark = SparkSession.builder \
                     .master("local") \
@@ -65,11 +97,11 @@ def main():
     events=spark.read.option("basePath", base_input_path).parquet(*paths)
 
     #рассчитываем необходимые датасеты с помощью функций 
-    common_subs_distance1=get_common_subs_distance(events)
+    common_subs_distance_zone1=get_common_subs_distance_zone(events, csv_path, spark)
     no_contacts_users1=get_no_contacts(events)
     
     #рассчитываем финальный датасет
-    recommendation=common_subs_distance1.join(no_contacts_users1, ['user_left', 'user_right'], 'inner')\
+    recommendation=common_subs_distance_zone1.join(no_contacts_users1, ['user_left', 'user_right'], 'inner')\
         .withColumn('timezone',  F.lit('Australia/Sydney'))\
         .withColumn("processed_dttm", current_date())\
         .withColumn('local_datetime',  F.from_utc_timestamp(F.col("processed_dttm"),F.col('timezone')))\
@@ -83,26 +115,33 @@ def main():
 #         .mode("overwrite") \
 #         .parquet(output_path)
                 
-def get_common_subs_distance(events):
+def get_common_subs_distance_zone(events, csv_path, spark):
                     
     #рассчитываем все пары пользователей, которые подписаны на один канал               
+    subs_user_left=events.filter(F.col('event_type')=='subscription')\
+    .select(F.col('event.user').alias('user_left'), F.col('lat').alias('lat_1').cast(DoubleType()),
+            F.col('lon').alias('lon_1').cast(DoubleType()), 'event.subscription_channel')\
+    .filter('lat_1 is not null and lon_1 is not null')
+  
+
     subs_user_right=events.filter(F.col('event_type')=='subscription')\
-    .select(F.col('event.user').alias('user_left'), F.col('lat').alias('lat_1'),
-            F.col('lon').alias('lon_1'), 'event.channel_id')
-
-    subs_user_left=subs_user_right.select(F.col('user_left').alias('user_right'), 
-                                          F.col('lat_1').alias('lat_2'), F.col('lon_1').alias('lon_2'), 'channel_id')
-
-    common_subs=subs_user_right.join(subs_user_left, 'channel_id' ,'inner')\
+    .select(F.col('event.user').alias('user_right'), 
+            F.col('lat').alias('lat_2').cast(DoubleType()), F.col('lon').alias('lon_2').cast(DoubleType()), 'event.subscription_channel')\
+    .filter('lat_2 is not null and lon_2 is not null')
+    
+    common_subs=subs_user_left.join(subs_user_right, 'subscription_channel' ,'inner')\
     .where(F.col('user_left')!=F.col('user_right')).distinct()
     
     #рассчитываем все пары пользователей, которые подписаны на один канал  и находятся менее, чем на 1 км друг от друга                
     common_subs_distance=common_subs\
-    .withColumn('distance', udf_func( F.col('lat_1'), F.col('lat_2'), F.col('lon_1'), F.col('lon_2')).cast('float'))\
-    .where((F.col('distance').isNotNull())&(F.col('distance')<1.0))\
-    .select('user_left','user_right')
+    .withColumn('distance', udf_func( F.col('lat_1'), F.col('lat_2'), F.col('lon_1'), F.col('lon_2')).cast(DoubleType()))\
+    .where((F.col('distance').isNotNull())&(F.col('distance')<1.0))
+    
+    #рассчитываем через функцию get_subs_city город (zone_id) - достаточно только для одного пользователя user_left
+    common_subs_distance_zone=get_subs_city(common_subs_distance,csv_path, spark)
+    common_subs_distance_zone.show()
            
-    return common_subs_distance
+    return common_subs_distance_zone
 
 def get_no_contacts(events):
            
@@ -137,5 +176,4 @@ def get_no_contacts(events):
 if __name__ == "__main__":
 
     main()
-   
      
