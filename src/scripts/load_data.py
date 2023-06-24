407a348249aa1ca9cf3bdd17f4ba2daaf210ce19
Voltschok <65617752+Voltschok@users.noreply.github.com>
2023-06-24 23:19:02 +0300

    Update load_data.py

--- src/scripts/load_data.py
+++ src/scripts/load_data.py
@@ -14,7 +14,7 @@ def main():
         base_input_path=sys.argv[1]
         date=sys.argv[2]
         depth=int(sys.argv[3])
-        output_path=sys.argv[5]
+        output_path=sys.argv[4]
 
         #base_input_path = '/user/master/data/geo/events'
         #date = '2022-05-25'
