4df66d0dfc20ede2095d1e6e0475db173700d2e4
Voltschok <65617752+Voltschok@users.noreply.github.com>
2023-06-26 22:25:03 +0300

    Update 3d_vitrin.py

--- src/scripts/3d_vitrin.py
+++ src/scripts/3d_vitrin.py
@@ -42,7 +42,9 @@ def input_paths(date, depth,base_input_path ):
 
 
 def get_geo_cities(csv_path, spark):
-    """Функция рассчитывает датасет с геоданными городов из csv-файла""" 
+    """Функция рассчитывает датасет с геоданными городов из csv-файла 
+    (в который уже добавлена колонка 'timezone' - вручную добавлены значения)""" 
+    
     geo_data_csv=spark.read.option("header", True)\
     .option("delimiter", ",").csv(csv_path)
  
